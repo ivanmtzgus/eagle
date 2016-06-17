1f95ad482382df4dbd31781cadaa1612017f0138
Ivan Martinez <ivanmtzgus2@hotmail.com>
2016-06-16 20:18:17 -0500

    add reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160616012036) do
+ActiveRecord::Schema.define(version: 20160617010858) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20160616012036) do
     t.datetime "updated_at", null: false
     t.string   "avatar"
     t.string   "image"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
